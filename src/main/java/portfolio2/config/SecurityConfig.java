056ffa4ed7ece17cc3b9270e1d3c9d4c949ca341
taehee-kim-dev <taehee.kim.dev@gmail.com>
2020-04-14 00:34:09 +0900

    Complete making sign-up.html

--- src/main/java/portfolio2/config/SecurityConfig.java
+++ src/main/java/portfolio2/config/SecurityConfig.java
@@ -27,6 +27,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(WebSecurity web) throws Exception {
         web.ignoring()
+                .mvcMatchers("/library/**")
                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 }
