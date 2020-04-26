7fc31864b3bc6268a5d97cca498fdc8c1e048d75
taehee-kim-dev <taehee.kim.dev@gmail.com>
2020-04-26 12:16:44 +0900

    Test showing page to resend email-check email

--- src/test/java/portfolio2/signup/ResendEmailCheckEmailTest.java
+++ src/test/java/portfolio2/signup/ResendEmailCheckEmailTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import portfolio2.WithAccount;
 import portfolio2.domain.account.Account;
 import portfolio2.domain.account.AccountRepository;
 
@@ -34,24 +35,12 @@ public class ResendEmailCheckEmailTest {
         accountRepository.deleteAll();
     }
 
+    private final String TEST_USER_ID = "testUserId";
+
     @DisplayName("이메일 인증 이메일 재전송 화면 보여주기")
+    @WithAccount(TEST_USER_ID)
     @Test
     void showResendEmailCheckEmailPage() throws Exception {
-
-        Account newAccountToSignUp = Account.builder()
-                .userId("testUserId")
-                .nickname("testNickname")
-                .email("test@email.com")
-                .password("testPassword")
-                .build();
-
-        mockMvc.perform(post("/sign-up")
-                .param("userId", newAccountToSignUp.getUserId())
-                .param("nickname", newAccountToSignUp.getNickname())
-                .param("email", newAccountToSignUp.getEmail())
-                .param("password", newAccountToSignUp.getPassword())
-                .with(csrf()))
-                .andExpect(authenticated());
 
         mockMvc.perform(get("/check-email"))
                 .andExpect(status().isOk())
@@ -60,115 +49,6 @@ public class ResendEmailCheckEmailTest {
                 .andExpect(model().attributeExists("email"))
                 .andExpect(view().name("account/check-email"));
 
-
     }
-
-    @DisplayName("회원가입 이메일 인증 - 유효한 링크")
-    @Test
-    void validEmailCheckToken() throws Exception {
-
-        Account newAccountToSignUp = Account.builder()
-                .userId("testUserId")
-                .nickname("testNickname")
-                .email("test@email.com")
-                .password("testPassword")
-                .build();
-
-        mockMvc.perform(post("/sign-up")
-                .param("userId", newAccountToSignUp.getUserId())
-                .param("nickname", newAccountToSignUp.getNickname())
-                .param("email", newAccountToSignUp.getEmail())
-                .param("password", newAccountToSignUp.getPassword())
-                .with(csrf()))
-                .andExpect(authenticated());
-
-        Account newAccountInDb = accountRepository.findByUserId(newAccountToSignUp.getUserId());
-
-        mockMvc.perform(get("/check-email-token")
-                .param("token", newAccountInDb.getEmailCheckToken())
-                .param("email", newAccountInDb.getEmail()))
-                .andExpect(status().isOk())
-                .andExpect(model().hasNoErrors())
-                .andExpect(view().name("account/checked-email"))
-                .andExpect(model().attributeExists("nickname"))
-                .andExpect(authenticated());
-
-        Account newAccountInDbAfterCompleteSignUp = accountRepository.findByUserId(newAccountToSignUp.getUserId());
-
-        assertTrue(newAccountInDbAfterCompleteSignUp.isEmailVerified());
-
-    }
-
-
-    @DisplayName("회원가입 이메일 인증 - 유효하지 않은 토큰")
-    @Test
-    void invalidEmailCheckToken() throws Exception {
-
-        Account newAccountToSignUp = Account.builder()
-                .userId("testUserId")
-                .nickname("testNickname")
-                .email("test@email.com")
-                .password("testPassword")
-                .build();
-
-        mockMvc.perform(post("/sign-up")
-                .param("userId", newAccountToSignUp.getUserId())
-                .param("nickname", newAccountToSignUp.getNickname())
-                .param("email", newAccountToSignUp.getEmail())
-                .param("password", newAccountToSignUp.getPassword())
-                .with(csrf()));
-
-        Account newAccountInDb = accountRepository.findByUserId(newAccountToSignUp.getUserId());
-
-        mockMvc.perform(get("/check-email-token")
-                .param("token", newAccountInDb.getEmailCheckToken() + "d")
-                .param("email", newAccountInDb.getEmail()))
-                .andExpect(status().isOk())
-                .andExpect(model().attribute("error", "invalidToken"))
-                .andExpect(view().name("account/checked-email"))
-                .andExpect(model().attributeDoesNotExist("nickname"))
-                .andExpect(unauthenticated());
-
-        Account newAccountInDbAfterCompleteSignUp = accountRepository.findByUserId(newAccountToSignUp.getUserId());
-
-        assertFalse(newAccountInDbAfterCompleteSignUp.isEmailVerified());
-
-    }
-
-    @DisplayName("회원가입 이메일 인증 - 유효하지 않은 이메일")
-    @Test
-    void invalidEmailCheckEmail() throws Exception {
-
-        Account newAccountToSignUp = Account.builder()
-                .userId("testUserId")
-                .nickname("testNickname")
-                .email("test@email.com")
-                .password("testPassword")
-                .build();
-
-        mockMvc.perform(post("/sign-up")
-                .param("userId", newAccountToSignUp.getUserId())
-                .param("nickname", newAccountToSignUp.getNickname())
-                .param("email", newAccountToSignUp.getEmail())
-                .param("password", newAccountToSignUp.getPassword())
-                .with(csrf()));
-
-        Account newAccountInDb = accountRepository.findByUserId(newAccountToSignUp.getUserId());
-
-        mockMvc.perform(get("/check-email-token")
-                .param("token", newAccountInDb.getEmailCheckToken())
-                .param("email", newAccountInDb.getEmail() + "d"))
-                .andExpect(status().isOk())
-                .andExpect(model().attribute("error", "invalidEmail"))
-                .andExpect(view().name("account/checked-email"))
-                .andExpect(model().attributeDoesNotExist("nickname"))
-                .andExpect(unauthenticated());
-
-        Account newAccountInDbAfterCompleteSignUp = accountRepository.findByUserId(newAccountToSignUp.getUserId());
-
-        assertFalse(newAccountInDbAfterCompleteSignUp.isEmailVerified());
-
-    }
-
 
 }
--- src/test/java/portfolio2/signup/SignUpEmailCheckTest.java
+++ src/test/java/portfolio2/signup/SignUpEmailCheckTest.java
@@ -131,4 +131,7 @@ public class SignUpEmailCheckTest {
     }
 
 
+
+
+
 }
