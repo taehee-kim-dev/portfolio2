853429223275a8e91ca9705d1ec27a8174d92ea9
taehee-kim-dev <taehee.kim.dev@gmail.com>
2020-04-26 10:06:46 +0900

    Test userId field input when sign-up

--- src/main/java/portfolio2/validator/SignUpRequestDtoValidator.java
+++ src/main/java/portfolio2/validator/SignUpRequestDtoValidator.java
@@ -29,43 +29,43 @@ public class SignUpRequestDtoValidator implements Validator {
         String emailPattern = "^[\\w!#$%&’*+/=?`{|}~^-]+(?:\\.[\\w!#$%&’*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$";
 
         if(signUpRequestDto.getUserId().length() < 5){
-            errors.rejectValue("userId", "invalidUserId", new Object[]{signUpRequestDto.getUserId()}, "아이디는 5자 이상이어야 합니다.");
+            errors.rejectValue("userId", "tooShortUserId","아이디는 5자 이상이어야 합니다.");
         }else if(signUpRequestDto.getUserId().length() > 20){
-            errors.rejectValue("userId", "invalidUserId", new Object[]{signUpRequestDto.getUserId()}, "아이디는 20자 이내여야 합니다.");
+            errors.rejectValue("userId", "tooLongUserId","아이디는 20자 이내여야 합니다.");
         }else if(!(signUpRequestDto.getUserId().matches(userIdPattern))){
-            errors.rejectValue("userId", "invalidUserId", new Object[]{signUpRequestDto.getUserId()}, "형식에 맞지 않는 아이디 입니다.");
+            errors.rejectValue("userId", "invalidFormatUserId", "형식에 맞지 않는 아이디 입니다.");
         }
 
         if(signUpRequestDto.getNickname().length() < 3){
-            errors.rejectValue("nickname", "invalidNickname", new Object[]{signUpRequestDto.getNickname()}, "닉네임은 3자 이상이어야 합니다.");
+            errors.rejectValue("nickname", "invalidNickname", "닉네임은 3자 이상이어야 합니다.");
         }else if(signUpRequestDto.getNickname().length() >15){
-            errors.rejectValue("nickname", "invalidNickname", new Object[]{signUpRequestDto.getNickname()}, "닉네임은 15자 이내여야 합니다.");
+            errors.rejectValue("nickname", "invalidNickname", "닉네임은 15자 이내여야 합니다.");
         }else if(!(signUpRequestDto.getNickname().matches(nicknamePattern))){
-            errors.rejectValue("nickname", "invalidNickname", new Object[]{signUpRequestDto.getNickname()}, "형식에 맞지 않는 닉네임 입니다.");
+            errors.rejectValue("nickname", "invalidNickname", "형식에 맞지 않는 닉네임 입니다.");
         }
 
         if(!(signUpRequestDto.getEmail().matches(emailPattern))){
-            errors.rejectValue("email", "invalidEmail", new Object[]{signUpRequestDto.getEmail()}, "이메일 형식에 맞지 않습니다.");
+            errors.rejectValue("email", "invalidEmail", "이메일 형식에 맞지 않습니다.");
         }
 
         if(signUpRequestDto.getPassword().length() < 8){
-            errors.rejectValue("password", "invalidPassword", new Object[]{signUpRequestDto.getPassword()}, "비밀번호는 8자 이상이어야 합니다.");
+            errors.rejectValue("password", "invalidPassword", "비밀번호는 8자 이상이어야 합니다.");
         }else if(signUpRequestDto.getPassword().length() >30){
-            errors.rejectValue("password", "invalidPassword", new Object[]{signUpRequestDto.getPassword()}, "비밀번호는 30자 이내여야 합니다.");
+            errors.rejectValue("password", "invalidPassword", "비밀번호는 30자 이내여야 합니다.");
         }else if((signUpRequestDto.getPassword().contains(" "))){
-            errors.rejectValue("password", "invalidPassword", new Object[]{signUpRequestDto.getPassword()}, "비밀번호에 공백은 포함될 수 없습니다.");
+            errors.rejectValue("password", "invalidPassword","비밀번호에 공백은 포함될 수 없습니다.");
         }
 
         if(accountRepository.existsByUserId(signUpRequestDto.getUserId())){
-            errors.rejectValue("userId", "invalidUserId", new Object[]{signUpRequestDto.getUserId()}, "이미 사용중인 아이디 입니다.");
+            errors.rejectValue("userId", "userIdAlreadyExists", "이미 사용중인 아이디 입니다.");
         }
 
         if(accountRepository.existsByNickname(signUpRequestDto.getNickname())){
-            errors.rejectValue("nickname", "invalidNickname", new Object[]{signUpRequestDto.getNickname()}, "이미 사용중인 닉네임 입니다.");
+            errors.rejectValue("nickname", "nicknameAlreadyExists", "이미 사용중인 닉네임 입니다.");
         }
         
         if(accountRepository.existsByEmail(signUpRequestDto.getEmail())){
-            errors.rejectValue("email", "invalidEmail", new Object[]{signUpRequestDto.getEmail()}, "이미 사용중인 이메일 입니다.");
+            errors.rejectValue("email", "invalidEmail", "이미 사용중인 이메일 입니다.");
         }
     }
 }
--- src/test/java/portfolio2/SignUpTest.java
+++ src/test/java/portfolio2/SignUpTest.java
@@ -1,13 +1,17 @@
 package portfolio2;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import portfolio2.domain.account.Account;
+import portfolio2.domain.account.AccountRepository;
 import portfolio2.dto.SignUpRequestDto;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -18,12 +22,108 @@ public class SignUpTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @AfterEach
+    void afterEach(){
+        accountRepository.deleteAll();
+    }
+
     @DisplayName("회원가입 화면 보여주기")
     @Test
     void showSignUpPage() throws Exception{
         mockMvc.perform(get("/sign-up"))
                 .andExpect(status().isOk())
                 .andExpect(model().hasNoErrors())
+                .andExpect(model().attributeExists("signUpRequestDto"))
+                .andExpect(view().name("account/sign-up"));
+    }
+
+
+    // userId errors.
+    @DisplayName("회원가입 POST 요청 - 너무 짧은 userId 길이 에러")
+    @Test
+    void signUpTooShortIdError() throws Exception{
+
+        mockMvc.perform(post("/sign-up")
+                .param("userId", "ab")
+                .param("nickname", "testNickname")
+                .param("email", "test@email.com")
+                .param("password", "12345678")
+                .with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(model().hasErrors())
+                .andExpect(model().attributeHasFieldErrorCode(
+                        "signUpRequestDto",
+                        "userId",
+                        "tooShortUserId"))
+                .andExpect(model().attributeExists("signUpRequestDto"))
+                .andExpect(view().name("account/sign-up"));
+    }
+
+    @DisplayName("회원가입 POST 요청 - 너무 긴 userId 길이 에러")
+    @Test
+    void signUpTooLongIdError() throws Exception{
+
+        mockMvc.perform(post("/sign-up")
+                .param("userId", "abcdeabcdeabcdeabcdeab")
+                .param("nickname", "testNickname")
+                .param("email", "test@email.com")
+                .param("password", "12345678")
+                .with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(model().hasErrors())
+                .andExpect(model().attributeHasFieldErrorCode(
+                        "signUpRequestDto",
+                        "userId",
+                        "tooLongUserId"))
+                .andExpect(model().attributeExists("signUpRequestDto"))
+                .andExpect(view().name("account/sign-up"));
+    }
+
+    @DisplayName("회원가입 POST 요청 - 형식에 맞지 않는 userId 에러")
+    @Test
+    void signUpInvalidFormatIdError() throws Exception{
+
+        mockMvc.perform(post("/sign-up")
+                .param("userId", "sdf df")
+                .param("nickname", "testNickname")
+                .param("email", "test@email.com")
+                .param("password", "12345678")
+                .with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(model().hasErrors())
+                .andExpect(model().attributeHasFieldErrorCode(
+                        "signUpRequestDto",
+                        "userId",
+                        "invalidFormatUserId"))
+                .andExpect(model().attributeExists("signUpRequestDto"))
+                .andExpect(view().name("account/sign-up"));
+    }
+
+    @DisplayName("회원가입 POST 요청 - 이미 존재하는 userId 에러")
+    @Test
+    void signUpIdAlreadyExistsError() throws Exception{
+
+        Account existingAccount = accountRepository.save(Account.builder()
+                .userId("testUserId")
+                .email("test@email.com")
+                .nickname("testNickname")
+                .build());
+
+        mockMvc.perform(post("/sign-up")
+                .param("userId", existingAccount.getUserId())
+                .param("nickname", "testNickname1")
+                .param("email", "test1@email.com")
+                .param("password", "12345678")
+                .with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(model().hasErrors())
+                .andExpect(model().attributeHasFieldErrorCode(
+                        "signUpRequestDto",
+                        "userId",
+                        "userIdAlreadyExists"))
                 .andExpect(model().attributeExists("signUpRequestDto"))
                 .andExpect(view().name("account/sign-up"));
     }
