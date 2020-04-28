00717e8934df3fd1f65541a7614bb3c00f6af567
taehee-kim-dev <taehee.kim.dev@gmail.com>
2020-04-28 21:35:12 +0900

    Test updating profile

--- src/test/java/portfolio2/account/LogInAndOutTest.java
+++ src/test/java/portfolio2/account/LogInAndOutTest.java
@@ -16,8 +16,7 @@ import static org.springframework.security.test.web.servlet.response.SecurityMoc
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
