8213a34434a475e1bb3c5f512dbfb4feba85ec70
taehee-kim-dev <taehee.kim.dev@gmail.com>
2020-07-14 01:44:23 +0900

    Test SecurityContextHolder

--- src/test/java/portfolio2/account/login/HomeAndLogInAndOutTest.java
+++ src/test/java/portfolio2/account/login/HomeAndLogInAndOutTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.web.servlet.MockMvc;
 import portfolio2.account.config.*;
 import portfolio2.domain.account.Account;
