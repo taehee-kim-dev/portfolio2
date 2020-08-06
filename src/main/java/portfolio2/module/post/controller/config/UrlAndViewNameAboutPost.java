92b1fd2b909f44dec15fe2726522e25f15082269
taehee-kim-dev <taehee.kim.dev@gmail.com>
2020-08-06 22:09:56 +0900

    Fix template naming bug

--- src/main/java/portfolio2/module/post/controller/config/UrlAndViewNameAboutPost.java
+++ src/main/java/portfolio2/module/post/controller/config/UrlAndViewNameAboutPost.java
@@ -4,13 +4,13 @@ public class UrlAndViewNameAboutPost {
 
     // PostController.java
     public static final String POST_NEW_POST_URL = "/post-new-post";
-    public static final String POST_NEW_POST_FORM_VIEW_NAME = "/post/form/post-new-post-form";
+    public static final String POST_NEW_POST_FORM_VIEW_NAME = "post/form/post-new-post-form";
 
     public static final String POST_VIEW_URL = "/post-view";
     public static final String POST_VIEW_NAME = "post/post-view";
 
     public static final String POST_UPDATE_URL = "/post-update";
-    public static final String POST_UPDATE_FORM_VIEW_NAME = "/post/form/post-update-form";
+    public static final String POST_UPDATE_FORM_VIEW_NAME = "post/form/post-update-form";
 
     public static final String POST_DELETE_URL = "/post-delete";
 }
