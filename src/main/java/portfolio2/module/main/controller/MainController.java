8d67ea0c86043b81b4974c8d38767e5bb369b9a5
taehee-kim-dev <taehee.kim.dev@gmail.com>
2020-08-06 15:05:30 +0900

    Make index.html

--- src/main/java/portfolio2/module/main/controller/MainController.java
+++ src/main/java/portfolio2/module/main/controller/MainController.java
@@ -8,7 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import portfolio2.module.account.Account;
 import portfolio2.module.account.config.SessionAccount;
-import portfolio2.module.notification.Notification;
+import portfolio2.module.post.PostRepository;
 
 import static portfolio2.module.main.config.UrlAndViewNameAboutMain.HOME_URL;
 import static portfolio2.module.main.config.UrlAndViewNameAboutMain.HOME_VIEW_NAME;
@@ -18,9 +18,12 @@ import static portfolio2.module.main.config.VariableNameAboutMain.SESSION_ACCOUN
 @Controller
 public class MainController {
 
+    private final PostRepository postRepository;
+
     @GetMapping(HOME_URL)
     public String home(@SessionAccount Account sessionAccount, Model model){
         model.addAttribute(SESSION_ACCOUNT, sessionAccount);
+        model.addAttribute("postList", postRepository.findFirst15ByOrderByFirstWrittenDateTimeDesc());
         return HOME_VIEW_NAME;
     }
 
